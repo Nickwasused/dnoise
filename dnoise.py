079ee375127f99bc26ec128cf13755e269cfa012
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2020-12-03 15:03:16 +0100

    --update

--- dnoise.py
+++ dnoise.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from importlib import reload
-import os
-import sys
-import time
+import datetime
 import json
-import codecs
-import pandas
-import urllib
+import os
 import random
 import sqlite3
-import datetime
-import requests
+import sys
+import time
+import urllib
+from importlib import reload
+
 import dns.resolver
+import pandas
+import requests
 
 reload(sys)
 
@@ -34,125 +34,127 @@ client = "127.0.0.1"
 dns.resolver.nameservers = "127.0.0.1"
 
 # Logging to a file. For easier debugging uncomment the second row.
-logfiletemp = working_directory + "dnoise.log"
-log_file = codecs.open(logfiletemp, mode="w", encoding="utf-8")
-#log_file = sys.stdout
+log_file = sys.stdout
+
 
 #				  END OF CONFIG SECTION  				#
 #########################################################################################
 
 def download_domains():
-	
-	start_time = time.time()
-	
-	# Download the Cisco Umbrella list. More info: https://s3-us-west-1.amazonaws.com/umbrella-static/index.html
-	try:
-		print("Downloading the domain list…")
-		urllib.urlretrieve("http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip", filename=working_directory+"domains.zip")
-	except:
-		print("Can't download the domain list. Quitting.")
-		exit()
-	
-	# Create a SQLite database and import the domain list
-	try:
-		db = sqlite3.connect(working_directory + "domains.sqlite")
-		db.execute("CREATE TABLE Domains (ID INT PRIMARY KEY, Domain TEXT)")
-		
-		# Load the CSV into our database
-		print("Importing to sqlite…")
-		df = pandas.read_csv(working_directory + "domains.zip", compression = 'zip', names = ["ID", "Domain"])
-		df.to_sql("Domains", db, if_exists = "append", index = False)
-	
-		db.close()
-	
-		os.remove(working_directory + "domains.zip")
-	except:
-		print("Import failed. Quitting.")
-		exit()
-	
-	# Running this on 1st gen Raspberry Pi can take up to 10 minutes. Be patient.
-	print"Done.")
+    # Download the Cisco Umbrella list. More info: https://s3-us-west-1.amazonaws.com/umbrella-static/index.html
+    try:
+        print("Downloading the domain list…")
+        urllib.request.urlretrieve("http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip",
+                           filename=working_directory + "domains.zip")
+    except Exception as e:
+        print(e)
+        print("Can't download the domain list. Quitting.")
+        exit()
+
+    # Create a SQLite database and import the domain list
+    try:
+        db = sqlite3.connect(working_directory + "domains.sqlite")
+        db.execute("CREATE TABLE Domains (ID INT PRIMARY KEY, Domain TEXT)")
+
+        # Load the CSV into our database
+        print("Importing to sqlite…")
+        df = pandas.read_csv(working_directory + "domains.zip", compression='zip', names=["ID", "Domain"])
+        df.to_sql("Domains", db, if_exists="append", index=False)
+
+        db.close()
+
+        os.remove(working_directory + "domains.zip")
+    except:
+        print("Import failed. Quitting.")
+        exit()
+
+    # Running this on 1st gen Raspberry Pi can take up to 10 minutes. Be patient.
+    print("Done.")
+
 
 # A simple loop that makes sure we have an Internet connection - it can take a while for pi-hole to get up and running after a reboot.
 while True:
-	try:
-		urllib.urlopen("https://github.com")
-		print("Got network connection.")
-		break
-	except:
-		print("Network not up yet, retrying in 10 seconds.")
-		time.sleep(10)
+    try:
+        urllib.request.urlopen("https://duckduckgo.com")
+        print("Got network connection.")
+        break
+    except Exception as e:
+        print(e)
+        print("Network not up yet, retrying in 10 seconds.")
+        time.sleep(10)
 
 # Download the top 1M domain list if we don't have it yet.
 exists = os.path.isfile(working_directory + "domains.sqlite")
-if exists == False:
-	download_domains()
+if not exists:
+    download_domains()
 
-db = sqlite3.connect(working_directory+"domains.sqlite")
+db = sqlite3.connect(working_directory + "domains.sqlite")
 
 while True:
-	# We want the fake queries to blend in with the organic traffic expected at each given time of the day, so instead of having a static delay between individual queries,
-	# we'll sample the network activity over the past 5 minutes and base the frequency on that. We want to add roughly 10% of additional activity in fake queries.
-	time_until = int(time.mktime(datetime.datetime.now().timetuple()))
-	time_from = time_until - 300
-	
-	# This will give us a list of all DNS queries that pi-hole handled in the past 5 minutes.
-	while True:
-		try:
-			all_queries = requests.get("http://pi.hole/admin/api.php?getAllQueries&from="+str(time_from)+"&until="+str(time_until)+"&auth="+auth)
-			break
-		except:
-			print(" API request failed. Retrying in 15 seconds.")
-			time.sleep(15)
-	
-	parsed_all_queries = json.loads(all_queries.text)
-	
-	# When determining the rate of DNS queries on the network, we don't want our past fake queries to skew the statistics, therefore we filter out queries made by this machine.
-	genuine_queries = []
-	try:
-		for a in parsed_all_queries["data"]:
-			if a[3] != client.replace("127.0.0.1","localhost"):
-				genuine_queries.append(a)
-	except:
-		print(" Pi-hole API response in wrong format. Investigate.")
-		exit()
-	
-	# Protection in case the pi-hole logs are empty.
-	if len(genuine_queries) == 0:
-		genuine_queries.append("Let's not devide by 0")
-	
-	# We want the types of our fake queries (A/AAA/PTR/…) to proportionally match those of the real traffic.
-	query_types = []
-	try:
-		for a in parsed_all_queries["data"]:
-			if a[3] != client.replace("127.0.0.1","localhost"):
-				query_types.append(a[1])
-	except:
-		print("Pi-hole API response in wrong format. Investigate.")
-		exit()
-	
-	# Default to A request if pi-hole logs are empty
-	if len(query_types) == 0:
-		query_types.append("A")
-	
-	while True:
-		# Pick a random domain from the top 1M list
-		rand = str(random.randint(1,1000000))
-		cursor = db.cursor()
-		cursor.execute("SELECT Domain FROM Domains WHERE ID="+rand)
-		domain = cursor.fetchone()[0]
-		
-		# Try to resolve the domain - that's why we're here in the first place, isn't it…
-		try:
-			dns.resolver.query(domain, random.choice(query_types))
-		except:
-			pass
-		
-		# We want to re-sample our "queries per last 5 min" rate every minute.
-		if int(time.mktime(datetime.datetime.now().timetuple())) - time_until > 60:
-			break
-		
-		# Since we want to add only about 10% of extra DNS queries, we multiply the wait time by 10, then add a small random delay.
-		time.sleep((300.0 / (len(genuine_queries)) * 10) + random.uniform(0,2))
-		
+    # We want the fake queries to blend in with the organic traffic expected at each given time of the day, so instead of having a static delay between individual queries,
+    # we'll sample the network activity over the past 5 minutes and base the frequency on that. We want to add roughly 10% of additional activity in fake queries.
+    time_until = int(time.mktime(datetime.datetime.now().timetuple()))
+    time_from = time_until - 300
+
+    # This will give us a list of all DNS queries that pi-hole handled in the past 5 minutes.
+    while True:
+        try:
+            all_queries = requests.get(
+                "http://pi.hole/admin/api.php?getAllQueries&from=" + str(time_from) + "&until=" + str(
+                    time_until) + "&auth=" + auth)
+            break
+        except:
+            print(" API request failed. Retrying in 15 seconds.")
+            time.sleep(15)
+
+    parsed_all_queries = json.loads(all_queries.text)
+
+    # When determining the rate of DNS queries on the network, we don't want our past fake queries to skew the statistics, therefore we filter out queries made by this machine.
+    genuine_queries = []
+    try:
+        for a in parsed_all_queries["data"]:
+            if a[3] != client.replace("127.0.0.1", "localhost"):
+                genuine_queries.append(a)
+    except:
+        print(" Pi-hole API response in wrong format. Investigate.")
+        exit()
+
+    # Protection in case the pi-hole logs are empty.
+    if len(genuine_queries) == 0:
+        genuine_queries.append("Let's not devide by 0")
+
+    # We want the types of our fake queries (A/AAA/PTR/…) to proportionally match those of the real traffic.
+    query_types = []
+    try:
+        for a in parsed_all_queries["data"]:
+            if a[3] != client.replace("127.0.0.1", "localhost"):
+                query_types.append(a[1])
+    except:
+        print("Pi-hole API response in wrong format. Investigate.")
+        exit()
+
+    # Default to A request if pi-hole logs are empty
+    if len(query_types) == 0:
+        query_types.append("A")
+
+    while True:
+        # Pick a random domain from the top 1M list
+        rand = str(random.randint(1, 1000000))
+        cursor = db.cursor()
+        cursor.execute("SELECT Domain FROM Domains WHERE ID=" + rand)
+        domain = cursor.fetchone()[0]
+
+        # Try to resolve the domain - that's why we're here in the first place, isn't it…
+        try:
+            dns.resolver.query(domain, random.choice(query_types))
+        except:
+            pass
+
+        # We want to re-sample our "queries per last 5 min" rate every minute.
+        if int(time.mktime(datetime.datetime.now().timetuple())) - time_until > 60:
+            break
+
+        # Since we want to add only about 10% of extra DNS queries, we multiply the wait time by 10, then add a small random delay.
+        time.sleep((300.0 / (len(genuine_queries)) * 10) + random.uniform(0, 2))
+
 db.close()
